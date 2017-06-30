37f6728b3e578474fdb40e52e43f134b94dc9260
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-29 21:52:03 -0400

    initial

---
+++ src/next-update-travis-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const nextUpdateTravis = require('.')
+
+describe('next-update-travis', () => {
+  it('write this test', () => {
+    console.assert(nextUpdateTravis, 'should export something')
+  })
+})
